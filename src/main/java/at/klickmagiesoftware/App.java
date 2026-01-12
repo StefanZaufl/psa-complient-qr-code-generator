fcdb4e9de34aecfefed6097f58f6ebcc40b175ac
Stefan Zaufl <stefan.zaufl@gmail.com>
2026-01-12 23:27:42 +0100

    Initialize Maven Project

---
+++ src/main/java/at/klickmagiesoftware/App.java
@@ -0,0 +1,13 @@
+package at.klickmagiesoftware;
+
+/**
+ * TODO: Remove Dummy Class
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/at/klickmagiesoftware/AppTest.java
@@ -0,0 +1,13 @@
+package at.klickmagiesoftware;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//TODO: Remove dummy Test
+class AppTest {
+    @Test
+    void main() {
+        assertTrue(true);
+    }
+}
